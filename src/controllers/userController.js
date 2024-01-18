147dfcd3d2ddd0cac29a073fd59ab6ad5fecf57b
Renard Bergson <renardrock@gmail.com>
2024-01-18 17:55:52 -0300

    testing controllers in vercel

---
+++ src/controllers/userController.js
@@ -0,0 +1,7 @@
+function getUsers (req, res) {
+  res.send('esta é a rota de usuários')
+}
+
+module.exports = {
+  getUsers
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
+const userController = require('../controllers/userController')
+
 router.get('/', (req, res) => {
-  res.send('está funcionando ainda!')
+  res.send('Bem-vindo ao servidor da maior plataforma de vendas do Brasil! 🎉 Anunx 🎊')
 })
+
+router.get('/users', userController.getUsers)
 
 module.exports = router
