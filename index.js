f7a3dbf26a0dc1d9f41320c68aea1903012b481a
Renard Bergson <renardrock@gmail.com>
2024-01-18 17:45:36 -0300

    testing router in vercel

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const routes = require('./src/routes/routes')
 
-app.get('/', (req, res) => {
-  res.send('está funcionando!')
-})
+app.use('/', routes)
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`server is listening on port ${port}`))
